aced8bb6c61e9aee8822b440a989c6694e4719b7
Ke Duong <kelyduong@gmail.com>
2024-03-12 18:33:18 +1100

    add GetDetailType

--- internal/pokeapi/apiStruct.go
+++ internal/pokeapi/apiStruct.go
@@ -35,3 +35,19 @@ type ApiPokemonType struct {
 	Slot int                 `json:"slot"`
 	Type ApiNamedAPIResource `json:"type"`
 }
+
+type ApiPokemonDetailedType struct {
+	Name            string                  `json:"name"`
+	DamageRelations ApiPokemonTypeRelations `json:"damage_relations"`
+}
+
+type ApiPokemonTypeRelations struct {
+	NoDamageTo   []ApiNamedAPIResource `json:"no_damage_to"`
+	NoDamageFrom []ApiNamedAPIResource `json:"no_damage_from"`
+
+	HalfDamageTo   []ApiNamedAPIResource `json:"half_damage_to"`
+	HalfDamageFrom []ApiNamedAPIResource `json:"half_damage_from"`
+
+	DoubleDamageTo   []ApiNamedAPIResource `json:"double_damage_to"`
+	DoubleDamageFrom []ApiNamedAPIResource `json:"double_damage_from"`
+}
--- internal/pokeapi/versus.go
+++ internal/pokeapi/versus.go
@@ -1,11 +1,90 @@
 package pokeapi
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"pokedex-cli/internal/utils/myAxios"
+)
 
-func Versus(attacker, defender string) error {
-	fmt.Println(attacker, defender)
+type DamageGuide struct {
+	MoveName    string
+	Recommended bool
+	Damage      int
+	Accuracy    int
+}
 
-	attackerMoves, err := GetPokemonMoves(attacker)
+func GetDetailType(pokemonType Type) (ApiPokemonDetailedType, error) {
+	println(pokemonType.Url)
+	var detailType ApiPokemonDetailedType
 
-	return nil
+	url := pokemonType.Url
+	data, err := myAxios.GetRequest(url)
+
+	if err != nil {
+		return ApiPokemonDetailedType{}, nil
+	}
+
+	json.Unmarshal(data, &detailType)
+
+	fmt.Printf("%v", detailType)
+
+	return ApiPokemonDetailedType{}, nil
+}
+
+func GetDamageGuide(moves []Move, targetTypes []Type) ([]DamageGuide, error) {
+	GetDetailType(targetTypes[0])
+
+	// var guide []DamageGuide
+	// // for _, move := range moves {
+	// url := fmt.Sprintf("https://pokeapi.co/api/v2/move/%s", moves[0])
+	// data, err := myAxios.GetRequest(url)
+
+	// if err != nil {
+	// 	println(err.Error())
+	// 	return nil, err
+	// }
+
+	// var apiPokemonDetailMove ApiPokemonDetailMove
+
+	// json.Unmarshal(data, &apiPokemonDetailMove)
+	// println(data)
+
+	// // }
+
+	// return guide, nil
+	return nil, nil
+}
+
+func Versus(attackerPokemon, defenderPokemon string) ([]DamageGuide, error) {
+	fmt.Println(attackerPokemon, defenderPokemon)
+
+	attacker, err := GetPokemon(attackerPokemon)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defender, err := GetPokemon(defenderPokemon)
+
+	if err != nil {
+		return nil, err
+	}
+
+	attackDamageGuide, err := GetDamageGuide(attacker.Moves, defender.Types)
+
+	if err != nil {
+		return nil, err
+	}
+
+	return attackDamageGuide, nil
+
+	// defenceDamageGuide, err := GetDamageGuide(defender.Moves, attacker.Types)
+
+	// if err != nil {
+	// 	return nil, err
+	// }
+
+	// // guide, err := TargetAgainst(attackerMoves, defender)
+
+	// return nil, nil
 }
